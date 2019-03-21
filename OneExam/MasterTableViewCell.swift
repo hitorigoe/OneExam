af4b5a6580c6e2b597289f0f25b1ea20a27e9769
new torigoe <newtorigoe@torigoehiroyukinoMacBook-Pro.local>
2019-03-21 17:26:56 +0900

    カスタムセル手前まで

---
+++ OneExam/MasterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MasterTableViewCell.swift
+//  OneExam
+//
+//  Created by new torigoe on 2019/03/21.
+//  Copyright © 2019 new torigoe. All rights reserved.
+//
+
+import UIKit
+
+class MasterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
