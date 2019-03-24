05de36978d2e3c8a12e1f94897787abbd4652320
鳥越洋之 <torigoe@torigoehiroyukinomacbook-pro.local>
2019-03-25 02:00:05 +0900

    結果ページのカスタムセルまで追加

--- OneExam/ResultTableViewCell.swift
+++ OneExam/ResultTableViewCell.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ResultTableViewCell: UITableViewCell {
-
+    var img : UIImage!
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        img = UIImage(named:"verified")
+        imgView.image = img
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- OneExam/ResultViewController.swift
+++ OneExam/ResultViewController.swift
@@ -7,11 +7,27 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
-class ResultViewController: UIViewController {
+class ResultViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        initTableView()
+        
+        let nib = UINib(nibName: "ResultTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "Cell")
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        // テーブル行の高さの概算値を設定しておく
+        // 高さ概算値 = 「縦横比1:1のUIImageViewの高さ(=画面幅)」+「いいねボタン、キャプションラベル、その他余白の高さの合計概算(=100pt)」
+        tableView.estimatedRowHeight = UIScreen.main.bounds.width
 
         // Do any additional setup after loading the view.
     }
@@ -26,5 +42,24 @@ class ResultViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得してデータを設定する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ResultTableViewCell
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension //変更
+    }
+    private func initTableView() {
+        tableView.register(UINib(nibName: "ResultTableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
+        tableView.estimatedRowHeight = 150 //追加
+        tableView.rowHeight = UITableView.automaticDimension //追加
+    }
 
 }
